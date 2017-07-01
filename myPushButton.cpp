17c0c5546889342877ad420bd66b52c5a6715fe6
skelstar <skelstar@gmail.com>
2017-07-02 05:32:57 +1200

    HeldLongLong push is wokring. Also Held_Seconds is working but not release. About to remove held params and just do held seconds.

--- myPushButton.cpp
+++ myPushButton.cpp
@@ -1,89 +1,144 @@
 #include "Arduino.h"
-#include "EventManager.h"
 #include "myPushButton.h"
-
-EventManager evM;
-#define EVENT_CODE EventManager::kEventUser9
 
 #define DOUBLETAP_MS_HIGH   1000
 #define DOUBLETAP_MS_LOW    200
+#define EVENT_CODE		0
 
 long lastButtonReleaseTime = 0;
 
+long _heldBeginMillis;
+long _heldDurationMillis;
+long _heldLongDurationMillis;
+int heldSecondsCounter;
+
 // declarations
 bool buttonReleaseInDoubletapWindow();
+void checkHeldDownSeconds();
+bool isHeldForLongEnough();
+bool isHeldForLongLongEnough();
 
-myPushButton::myPushButton(uint8_t pin, bool pullUp, uint16_t heldDurationMs, uint8_t lowState, EventListener listenerCallback) {
+//--------------------------------------------------------------------------------
+
+myPushButton::myPushButton(uint8_t pin, 
+						   bool pullUp, 
+						   uint16_t heldDurationMs, 
+						   uint16_t heldLongDurationMs, 
+						   uint8_t offState, 
+						   EventListener listenerCallback) {
 
     _pin = pin;
     _pullUp = pullUp;
-    _lowState = lowState;
+    _offState = offState;
     _heldDurationMillis = heldDurationMs;
+    _heldLongDurationMillis = heldLongDurationMs;
+    _listenerCallback = listenerCallback;
 
     _state = ST_NOT_HELD;
 
-    if (listenerCallback != NULL) {
-        evM.addListener(EVENT_CODE, listenerCallback);
-    }
-
-    init2();
+    init();
 }
 
 void myPushButton::serviceEvents() {
+
     bool buttonPressed = isPressed();
+    bool buttonReleased = !isPressed();
 
-    if (_state == ST_NOT_HELD) {
-        if (buttonPressed) {
-            _state = EV_BUTTON_PRESSED;
-        }
+	switch (_state) {
 
-    } else if (_state == EV_BUTTON_PRESSED ) {
-        _heldBeginMillis = millis();
-        evM.queueEvent(EVENT_CODE, _state);
-        _state = ST_WAIT_FOR_HELD_TIME;
+		case ST_NOT_HELD:
+			if (buttonPressed) {
+				//Serial.println("*** EV_BUTTON_PRESSED");
+				_state = EV_BUTTON_PRESSED;
+			}
+			break;
 
-    } else if (_state == ST_WAIT_FOR_HELD_TIME) {
-        if (buttonPressed && isHeldForLongEnough()) {
-            _state = EV_HELD_FOR_LONG_ENOUGH;
-        } else if (!buttonPressed) {
-            _state = EV_RELEASED;
-        }
+		case EV_BUTTON_PRESSED:
+			_heldBeginMillis = millis();
+			heldSecondsCounter = 1;
+			_listenerCallback(EVENT_CODE, EV_BUTTON_PRESSED);
+			// Serial.println("ST_WAIT_FOR_HELD_TIME");
+			_state = ST_WAIT_FOR_HELD_TIME;
+			break;
 
-    } else if (_state == EV_HELD_FOR_LONG_ENOUGH) {
-        evM.queueEvent(EVENT_CODE, _state);
-        _state = ST_WAITING_FOR_RELEASE_FROM_HELD_TIME;
-        lastButtonReleaseTime = 0;
+		case ST_WAIT_FOR_HELD_TIME:
+			if (buttonPressed && isHeldForLongEnough()) {
+				// Serial.println("*** EV_HELD_FOR_LONG_ENOUGH");
+				_state = EV_HELD_FOR_LONG_ENOUGH;
+			}
+			else if (buttonReleased) {
+				// Serial.println("EV_RELEASED");
+				_state = EV_RELEASED;
+			}
+			break;
 
-    } else if (_state == ST_WAITING_FOR_RELEASE) {
-        if (!buttonPressed) {
-            _state = EV_RELEASED;
-        }
-    
-    } else if (_state == ST_WAITING_FOR_RELEASE_FROM_HELD_TIME) {
-        if (!buttonPressed) {
-            _state = EV_RELEASED_FROM_HELD_TIME;
-        }
-    } 
+		case EV_HELD_FOR_LONG_ENOUGH:
+			_listenerCallback(EVENT_CODE, EV_HELD_FOR_LONG_ENOUGH);
+			// Serial.println("ST_WAITING_FOR_LONG_HELD_TIME");
+			_state = ST_WAITING_FOR_LONG_HELD_TIME;
+			lastButtonReleaseTime = 0;
+			break;
+
+		case ST_WAITING_FOR_LONG_HELD_TIME:
+			if (buttonPressed && isHeldForLongLongEnough()) {
+				// Serial.println("*** EV_HELD_FOR_LONG_LONG_ENOUGH");
+				_state = EV_HELD_FOR_LONG_LONG_ENOUGH;
+			}
+			else if (buttonReleased) {
+				// Serial.println("EV_RELEASED_FROM_HELD_TIME");
+				_state = EV_RELEASED_FROM_HELD_TIME;
+			}
+			break;
+
+		case EV_HELD_FOR_LONG_LONG_ENOUGH:
+			_listenerCallback(EVENT_CODE, EV_HELD_FOR_LONG_LONG_ENOUGH);
+			// Serial.println("ST_WAITING_FOR_RELEASE_FROM_HELD_LONG_TIME");
+			_state = ST_WAITING_FOR_RELEASE_FROM_HELD_LONG_TIME;
+			lastButtonReleaseTime = 0;
+			break;
+
+		case ST_WAITING_FOR_RELEASE_FROM_HELD_LONG_TIME:
+			if (buttonReleased) {
+				// Serial.println("EV_RELEASED_FROM_LONG_HELD_TIME");
+				_state = EV_RELEASED_FROM_LONG_HELD_TIME;
+			}
+			break;
+	}
 
     if (_state == EV_RELEASED) {
-        evM.queueEvent(EVENT_CODE, _state);
         if (buttonReleaseInDoubletapWindow()) {
-            evM.queueEvent(EVENT_CODE, EV_DOUBLETAP);
+            _listenerCallback(EVENT_CODE, EV_DOUBLETAP);
+			// Serial.println("*** EV_DOUBLETAP");
         	lastButtonReleaseTime = 0;
         } else {
             lastButtonReleaseTime = millis();
+			// Serial.println("ST_NOT_HELD");
+			_listenerCallback(EVENT_CODE, EV_RELEASED);
             _state = ST_NOT_HELD;
         }
-    } else if (_state == EV_RELEASED_FROM_HELD_TIME) {
-        evM.queueEvent(EVENT_CODE, _state);
-        lastButtonReleaseTime = millis();
-        _state = ST_NOT_HELD;
+    } 
+    else if (_state == EV_RELEASED_FROM_HELD_TIME ||
+		   	 _state == EV_RELEASED_FROM_LONG_HELD_TIME) {
+		_listenerCallback(EVENT_CODE, _state);
+		// Serial.println("ST_NOT_HELD");
+		_state = ST_NOT_HELD;
+		lastButtonReleaseTime = millis();
     }
 
-    evM.processEvent();
+    // EV_HELD_SECONDS
+    if (_state != ST_NOT_HELD && buttonPressed) {
+		long heldDownDuration = millis() - _heldBeginMillis;
+		if ((heldSecondsCounter)*1000 < heldDownDuration) {
+			if (heldSecondsCounter > 1) {
+				_listenerCallback(heldSecondsCounter-1, EV_HELD_SECONDS);
+				//Serial.print("EV_HELD_SECONDS_"); Serial.println(heldSecondsCounter);
+			}
+			heldSecondsCounter++;
+		}
+    }
 }
 
-void myPushButton::init2() {
+void myPushButton::init() {
     if (_pullUp) {
         pinMode(_pin, INPUT_PULLUP);
     } else {
@@ -94,12 +149,19 @@ void myPushButton::init2() {
 }
 
 bool myPushButton::isPressed() {
-    init2();
-    return digitalRead(_pin) != _lowState;
+    init();
+    return digitalRead(_pin) != _offState;
 }
 
-bool myPushButton::isHeldForLongEnough() {
+void checkHeldDownSeconds() {
+}
+
+bool isHeldForLongEnough() {
     return long(_heldBeginMillis + _heldDurationMillis) <= millis();
+}
+
+bool isHeldForLongLongEnough() {
+	return long(_heldBeginMillis + _heldLongDurationMillis) <= millis();
 }
 
 bool buttonReleaseInDoubletapWindow() {
--- myPushButton.h
+++ myPushButton.h
@@ -2,40 +2,42 @@
 #define myPushButton_h
 
 #include <Arduino.h>
-#include <EventManager.h>
 
 class myPushButton 
 {
     public:
         enum StateCode {
-            ST_NOT_HELD,
-            EV_BUTTON_PRESSED,
-            ST_WAIT_FOR_HELD_TIME,
-            EV_HELD_FOR_LONG_ENOUGH,
-            ST_WAITING_FOR_RELEASE,
-            ST_WAITING_FOR_RELEASE_FROM_HELD_TIME,
-            EV_RELEASED,
-            EV_RELEASED_FROM_HELD_TIME,
-            EV_DOUBLETAP
+			ST_NOT_HELD,
+			EV_BUTTON_PRESSED,
+			ST_WAIT_FOR_HELD_TIME,
+			EV_HELD_FOR_LONG_ENOUGH,
+			EV_HELD_FOR_LONG_LONG_ENOUGH,
+			ST_WAITING_FOR_LONG_HELD_TIME,
+			ST_WAITING_FOR_RELEASE_FROM_HELD_TIME,
+			ST_WAITING_FOR_RELEASE_FROM_HELD_LONG_TIME,
+			EV_RELEASED,
+			EV_RELEASED_FROM_HELD_TIME,
+			EV_RELEASED_FROM_LONG_HELD_TIME,
+			EV_RELEASED_SECONDS,
+			EV_HELD_SECONDS,
+			EV_DOUBLETAP
         };
 
         typedef void ( *EventListener )( int eventCode, int eventParam );
-        typedef void ( *ButtonListener )( int state, int eventParam );
-        myPushButton(uint8_t pin, bool pullUp, uint16_t heldDurationMs, uint8_t lowState, EventListener listenerCallback);
+
+        myPushButton(uint8_t pin, bool pullUp, uint16_t heldDurationMs, uint16_t heldLongDurationMs, uint8_t offState, EventListener listenerCallback);
         bool isPressed();
         void serviceEvents();
 
     private:
-        EventManager evM;
         uint8_t _pin;
         bool _pullUp;
         uint8_t _state;
-        uint8_t _lowState;
-        long _heldDurationMillis;
-        long _heldBeginMillis;
+		uint8_t _offState;
 
-        bool isHeldForLongEnough();
-        void init2();
+        EventListener _listenerCallback;
+
+        void init();
 };
 
 #endif
